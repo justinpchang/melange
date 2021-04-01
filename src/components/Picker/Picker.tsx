87e48707245c7e96cad9bd27761dd1dc4ebadb22
Justin Chang <justin.p.chang@gmail.com>
2021-03-31 23:45:04 -0400

    Use DeltaE color difference calculation

--- src/components/Picker/Picker.tsx
+++ src/components/Picker/Picker.tsx
@@ -8,7 +8,10 @@ import getCanvasPixelColor from 'get-canvas-pixel-color';
 
 import useCanvasStore from '../../stores/canvas';
 import useGameStore from '../../stores/game';
-import { rgbToHex } from '../../utils/colors';
+import {
+  rgbToHex,
+  calcColorDifference,
+} from '../../utils/colors';
 
 import {
   PickedColor,
@@ -17,10 +20,13 @@ import {
 const Picker: FC = (): ReactElement => {
   const canvas = useCanvasStore(state => state.canvas);
   const setCanvasDisabled = useCanvasStore(state => state.setDisabled);
+  const targetColor = useGameStore(state => state.targetColor);
   const pickedColor = useGameStore(state => state.pickedColor);
   const setPickedColor = useGameStore(state => state.setPickedColor);
+  const [displayColor, setDisplayColor] = useState('');
   const [pickingColor, setPickingColor] = useState(false);
   const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [accuracy, setAccuracy] = useState(0);
 
   const setPickingMode = (isPicking: boolean) => {
     if (document.body) {
@@ -35,19 +41,17 @@ const Picker: FC = (): ReactElement => {
 
   const stopPickingColor = () => setPickingMode(false);
 
-  const exitPickByKeyPress = (e: KeyboardEvent) => (e.key == "Escape") && setPickingMode(false);
-
   const pickColor = (e: MouseEvent) => {
     const rect = canvas?.getBoundingClientRect();
     const x = e.x - (rect?.x || 0);
     const y = e.y - (rect?.y || 0);
     const { r, g, b, a } = getCanvasPixelColor(canvas, x, y);
-    console.log(r, g, b, a);
-    setPickedColor(`rgba(${r}, ${g}, ${b}, ${a / 255})`);
+    setDisplayColor(`rgba(${r}, ${g}, ${b}, ${a / 255})`);
+    setPickedColor(rgbToHex(r, g, b));
     stopPickingColor();
   };
 
-  // Set up listener for listening
+  // Setup listener for click
   useEffect(() => {
     if (pickingColor) {
       document.addEventListener('click', pickColor);
@@ -57,12 +61,25 @@ const Picker: FC = (): ReactElement => {
     }
   }, [pickingColor]);
 
+  // Calculate accuracy
+  useEffect(() => {
+    if (pickedColor && targetColor) {
+      setAccuracy(100 - calcColorDifference(pickedColor.substring(1), targetColor.substring(1)));
+    }
+  }, [pickedColor, targetColor]);
+
   return (
     <div>
-      <h3>Picked:</h3>
-      <PickedColor color={pickedColor} />
-      <br />
-      <button onClick={startPickingColor} disabled={buttonDisabled}>Pick color</button>
+      <div style={{display: 'inline-block'}}>
+        <h3>Picked:</h3>
+        <PickedColor color={displayColor} />
+        <br />
+        <button onClick={startPickingColor} disabled={buttonDisabled}>Pick color</button>
+      </div>
+      <div style={{display: 'inline-block', border: '2px solid black', padding: '10px'}}>
+        <h3>Result:</h3>
+        <h1>{accuracy}%</h1>
+      </div>
     </div>
   );
 };
