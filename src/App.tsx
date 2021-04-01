7ca7fb7ce926d2fe133dbc37ecf9ac7e3c561008
Justin Chang <justin.p.chang@gmail.com>
2021-04-01 18:56:55 -0400

    Add instructions

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,12 @@ import {
 } from './App.styles';
 
 const App: FC = (): ReactElement => {
+  const showInstructions = () => {
+    alert('Page 1 out of 3\n\nTry to match the target color by mixing colors from your palette.');
+    alert('Page 2 out of 3\n\nWhen you are finished, click the eyedropper to select the color that you think is the best match.');
+    alert('Page 3 out of 3\n\nMultiplayer coming soon. Thanks for playing!');
+  };
+
   return (
     <div>
       <Header>
@@ -39,6 +45,8 @@ const App: FC = (): ReactElement => {
         <Footer>
           made by justin chang
           <br />
+          <a onClick={showInstructions}>instructions</a>
+          &nbsp;|&nbsp;
           <a href="https://github.com/justinpchang/melange">view source</a>
         </Footer>
       </MainContainer>
